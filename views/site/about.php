66dc4af10a6e22632dd9be8eb8e9caf497a06924
Eugene Fabrikov <eugene.fabrikov@gmail.com>
2016-07-05 11:03:30 +0300

    code

---
+++ views/site/about.php
@@ -0,0 +1,32 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        This is the About page. You may modify the following file to customize its content:
+    </p>
+
+    <code><?= __FILE__ ?></code>
+</div>
+<hr>
+<?php
+function createGreeter($who) {
+    return function() use ($who) {
+        echo 'Hello ' . $who;
+    };
+}
+
+$greeter = createGreeter("World");
+$greeter();
+
+?>
+
+<hr>
